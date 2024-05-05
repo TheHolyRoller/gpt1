e3afa8a724b3a391d08105c1f06ce684274d75f6
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2024-05-05 21:15:06 +0100

    Hero start

---
+++ app/Components/Hero.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react'
+
+function Hero() {
+  return (
+    <header id='heroContainer'>
+    
+    <div id='heroSubContainer'>
+
+    <div id='heroImageContainer' >
+
+    <div id='heroImage' ></div>
+
+
+    </div>
+
+    <div id='heroTextContainer'>
+
+    <h1 id='heroText'> Accelerate Your Ambition </h1>
+
+
+    </div>
+    
+    </div>
+    </header>
+  )
+}
+
+export default Hero
--- app/page.js
+++ app/page.js
@@ -3,16 +3,16 @@
 import Image from "next/image";
 import FileUpload from "./Components/FileUpload";
 import Nav from '../app/Components/Navbar'; 
+import Hero from '../app/Components/Hero'; 
 
 
 
 export default function Home() {
   return (
-    <main id='main' >
+    <main id='main'>
 
-  <Nav/>
-
-    
+     <Hero/> 
+        
     </main>
   );
 }
